50c502e75edc514f52269c81ffe9d03fe0ab4ad0
JulianG333 <juliancamacho2003@gmail.com>
2024-05-22 16:57:58 -0500

    Crear dirección

--- shipping_addresses/views.py
+++ shipping_addresses/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib import messages
 from .models import ShippingAddress
 from django.views.generic import ListView
+from .forms import ShippingAddressForm
 
 class ShippingAddressListView(ListView):
     model = ShippingAddress
@@ -10,8 +12,23 @@ class ShippingAddressListView(ListView):
         return ShippingAddress.objects.filter(user=self.request.user).order_by('-default')
     
 def create(request):
+    form = ShippingAddressForm(request.POST or None)
+
+    if request.method == 'POST' and form.is_valid():
+        shipping_address = form.save(commit=False)
+        shipping_address.user = request.user
+
+        shipping_address.default = not ShippingAddress.objects.filter(user=request.user).exists()
+
+        shipping_address.save()
+
+        messages.success(request, 'Dirección creada exitosamente')
+        return redirect('shipping_addresses:shipping_addresses')
+
     return render(request, 'shipping_address/create.html',{
-        
+        'form': form
     })
+
+
 
 
