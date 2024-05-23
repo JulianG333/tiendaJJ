f52c83386832f1fc1f094ea3f5ef0b109d9b4568
JulianG333 <juliancamacho2003@gmail.com>
2024-05-23 08:57:15 -0500

    Restringir direcciones

--- shipping_addresses/views.py
+++ shipping_addresses/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
 from .models import ShippingAddress
 from django.views.generic import ListView
 from .forms import ShippingAddressForm
 
-class ShippingAddressListView(ListView):
+class ShippingAddressListView(LoginRequiredMixin, ListView):
+    login_url = 'login'
     model = ShippingAddress
     template_name = 'shipping_address/shipping_address.html'
 
     def get_queryset(self):
         return ShippingAddress.objects.filter(user=self.request.user).order_by('-default')
     
+@login_required(login_url='login')
 def create(request):
     form = ShippingAddressForm(request.POST or None)
 
