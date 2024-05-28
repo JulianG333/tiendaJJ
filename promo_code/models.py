bfc4a98acf1a803b96e4ad628804619e602ea683
JulianG333 <juliancamacho2003@gmail.com>
2024-05-28 15:52:56 -0500

    Validar código promocional

--- promo_code/models.py
+++ promo_code/models.py
@@ -1,8 +1,14 @@
 import string
 import random
 from django.db import models
+from django.utils import timezone
 from django.db.models.signals import pre_save
 
+class PromoCodeManager(models.Manager):
+
+    def get_valid(self, code):
+        now = timezone.now()
+        return self.filter(code=code).filter(used=False).filter(valid_from__lte=now).filter(valid_to__gte=now).first()
 
 
 class PromoCode(models.Model):
@@ -12,6 +18,8 @@ class PromoCode(models.Model):
     valid_to = models.DateTimeField()
     used = models.BooleanField(default=False)
     create_at = models.DateField(auto_now_add=True)
+
+    objects = PromoCodeManager()
 
 
     def __str__(self): 
