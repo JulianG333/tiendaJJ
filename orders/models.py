7b7413483a80bdf02ad69d47f9937fe3ae1c0ddb
JulianG333 <juliancamacho2003@gmail.com>
2024-05-28 12:22:50 -0500

    Aplicar Código Promocional

--- orders/models.py
+++ orders/models.py
@@ -1,10 +1,12 @@
 import uuid
+import decimal
 from shipping_addresses.models import ShippingAddress
 from django.db.models.signals import pre_save
 from django.db import models
 from .common import OrderStatus, choices
 from users.models import User
 from carts.models import Cart
+from promo_code.models import PromoCode
 
 class Order(models.Model):
     order_id = models.CharField(max_length=100,null=False, blank=False, unique=True)
@@ -16,9 +18,19 @@ class Order(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     shipping_address = models.ForeignKey(ShippingAddress, null=True, blank=True, on_delete=models.CASCADE)
 
+    promo_code = models.OneToOneField(PromoCode, null=True, blank=True,
+                                      on_delete=models.CASCADE)
 
     def __str__(self):
         return self.order_id
+    
+    def apply_promo_code(self, promo_code ):
+        if self.promo_code is None:
+            self.promo_code =  promo_code
+            self.save()
+
+            self.update_total()
+            promo_code.use()
     
     def get_or_set_shipping_address(self):
         if self.shipping_address:
@@ -45,8 +57,17 @@ class Order(models.Model):
         self.total = self.get_total()
         self.save()
     
+    def get_discount(self):
+        if self.promo_code:
+            return self.promo_code.discount
+        
+        return 0
+
+
     def get_total(self):
-        return self.cart.total + self.shopping_total
+        return self.cart.total + self.shopping_total - decimal.Decimal(self.get_discount())
+    
+
     
 def set_order_id(sender, instance, *args, **kwargs):
     if not instance.order_id:
--- promo_code/views.py
+++ promo_code/views.py
@@ -1,15 +1,29 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from .models import PromoCode
+from orders.utils import get_or_create_order
+from carts.utils import get_or_create_cart
+
+
 
 def validate(request):
+    cart = get_or_create_cart(request)
+    order = get_or_create_order(cart, request)
+    code = request.GET.get('code')
+    promo_code = PromoCode.objects.filter(code=code).first()
+
+    if promo_code is None:
+        return JsonResponse({
+            'status':False
+        }, status=404 )
+
+    order.apply_promo_code(promo_code)
+
     return JsonResponse({
-        'name' : 'Julian',
-        'job' : 'GrupoASD ',
-        'courses': [
-            {'title': 'Python'},
-            {'title': 'Java'},
-            {'title': 'BD'},
-        ]
+        'status': True,
+        'code' : promo_code.code,
+        'discount' : promo_code.discount,
+        'total': order.total
     })
 
 
